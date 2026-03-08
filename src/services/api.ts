521b110dca7b4bb19629d9758038484690ee8ccb
henrytrust1111 <henrytrust1111@gmail.com>
2026-03-08 10:02:11 +0100

    added the hostd URL for the backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://calender-system-api.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
